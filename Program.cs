f5e69c32f4f67cf6fcb94bf1c0be1b325b722d51
Danil Tarnovskiy <tarnovskiy2002@gmail.com>
2022-03-17 12:00:25 +0200

    Uploaded all files.
    Uploaded all files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Serial_Port_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort mySerialPort = new SerialPort();
+            string[] ports;
+
+            while (!mySerialPort.IsOpen)
+            {
+                ports = SerialPort.GetPortNames();
+                Console.WriteLine("The following serial ports were found:\n");
+
+                foreach (string port in ports)
+                {
+                    Console.WriteLine(port);
+                }
+
+                Console.Write("\nChose a serial port: ");
+                try 
+                {
+                    mySerialPort = new SerialPort(Console.ReadLine(), 9600, Parity.None, 8, StopBits.One);
+                    Console.Clear();
+                    mySerialPort.Open();
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nChose another serial port\n\n");
+                }
+            }
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            Console.ReadLine();
+        }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine("Data Received: " + indata);
+        }
+    }
+}
